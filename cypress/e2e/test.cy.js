8a43a064d3a2abe893d991c73f9424bd5da287b6
Kevin <kevin.cuartero@gmail.com>
2023-07-20 20:50:22 -0400

    updated gateway

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,10 +1,9 @@
 describe('CRC E2E Test', () => {
   it('Check API to return OK status code and visitor count', () => {
-    cy.request('https://kcuartero-gw-5u3b1jgg.uc.gateway.dev/count')
+    cy.request('https://kcuartero-gw-cafxka52.ue.gateway.dev/count')
       .then((response) => {
       expect(response.status).to.eq(200) //check http response if OK(200)
       expect(response.body).to.have.property('count') //check if the API returns count value
     }) 
-  })
+  }) 
 })
-      
