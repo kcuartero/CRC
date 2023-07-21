bbc7ace23f730be16d9e8abb46d5a44eadbbcda2
Kevin <kevin.cuartero@gmail.com>
2023-07-20 20:47:50 -0400

    testing backend ci/cd

--- backend/main.py
+++ backend/main.py
@@ -24,4 +24,4 @@ def saveCount():
         return "Method not allow", 405
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080))) 
